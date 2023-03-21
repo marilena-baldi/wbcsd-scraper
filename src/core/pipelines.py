9c5c7215a7865214412547933fee88207e4d2ebe
marilena <marilenabaldi95@gmail.com>
2023-03-21 22:42:39 +0100

    added data validation pipeline

--- src/core/pipelines.py
+++ src/core/pipelines.py
@@ -1,13 +1,22 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+""" Scrapy pipelines """
 
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import dateparser
 
 
-class CorePipeline:
-    def process_item(self, item, spider):
+class ValidationPipeline:
+    """ The ValidationPipeline processes the scraped items validating its fields """
+
+    def process_item(self, item, spider): # pylint: disable=unused-argument
+        """ Validate the items stripping the string fields and setting them to None if they are
+            missing """
+
+        adapter = ItemAdapter(item)
+
+        adapter['title'] = adapter['title'].strip() if adapter['title'] else None
+        adapter['publication_date'] = dateparser.parse(
+                                        adapter['publication_date']
+                        ).strftime('%Y-%m-%d') if adapter['publication_date'] else None
+        adapter['tags'] = [tag.strip() for tag in adapter['tags']] if adapter['tags'] else None
+
         return item
--- src/core/settings.py
+++ src/core/settings.py
@@ -66,9 +66,9 @@ SPIDER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "core.pipelines.CorePipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "core.pipelines.ValidationPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
